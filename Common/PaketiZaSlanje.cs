1bb6e1a47b85566a932523038303743fe4d8bd09
teodora2410 <tceremidzic01@gmail.com>
2023-08-26 18:56:03 +0200

    dodate neophodne klase za rad i Interface za wcf

---
+++ Common/PaketiZaSlanje.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public enum TipBaze
+    {
+        XML,
+        INMEMORY
+    }
+
+    [DataContract]
+    public class PaketiZaSlanje : IDisposable
+    {
+        [DataMember]
+        public MemoryStream MS { get; set; }
+
+        [DataMember]
+        public string FileName { get; set; }
+
+        [DataMember]
+        public TipBaze TipBaze { get; set; }
+
+        public PaketiZaSlanje(MemoryStream ms, string fileName, TipBaze tipBaze)
+        {
+            this.MS = ms;
+            this.FileName = fileName;
+            this.TipBaze = tipBaze;
+        }
+
+        public void Dispose()
+        {
+            if (MS == null)
+                return;
+            try
+            {
+                MS.Dispose();
+                MS.Close();
+                MS = null;
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("Neuspesno ciscenje!");
+            };
+        }
+    }
+}
---
+++ Common/RezultatSlanja.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public enum TipRezultata
+    {
+        Uspesno,
+        Neuspesno
+    }
+    public class RezultatSlanja
+    {
+        [DataMember]
+        public string Poruka { get; set; }
+
+        [DataMember]
+        public TipRezultata TipRezultata { get; set; }
+
+        public RezultatSlanja()
+        {
+            this.TipRezultata = TipRezultata.Uspesno;
+        }
+    }
+}
